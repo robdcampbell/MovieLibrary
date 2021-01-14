feec5113f13283a3374f368ea5dada0caa4a4ea6
robdcampbell <rdeancampbell@gmail.com>
2021-01-14 11:44:28 -0500

    initial commit: Movie Library

---
+++ README.md
@@ -0,0 +1,17 @@
+# Movie Library (using React).
+
+## Technologies used:
+
+- React.js (context API, )
+
+## Project Summary:
+
+Fetching movie information from https://www.omdbapi.com/, this front-end projects displays movies that are searched by the visiting user.
+
+## Dependencies:
+
+After clone repo, change your command line directory to the project root folder and run:
+
+- npm install && npm start
+
+- to run this project on your own, you'll need to generate an API key and store it in a .env file within the "src" directory.
---
+++ src/SingleMovie.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { API_ENDPOINT } from "./context";
+import useFetch from "./useFetch";
+
+const SingleMovie = () => {
+  const { id } = useParams();
+  const { isLoading, error, data: movie } = useFetch(`&i=${id}`);
+
+  if (isLoading) {
+    return <div className="loading">'Loading</div>;
+  }
+  if (error.show) {
+    return (
+      <div className="page-error">
+        <h1>{error.msg}</h1>
+        <Link to="/" className="btm">
+          Back to Movies
+        </Link>
+      </div>
+    );
+  }
+  const { Poster: poster, Title: title, Plot: plot, Year: year } = movie;
+  return (
+    <section className="single-movie">
+      <img src={poster} alt={title} />
+      <div className="single-movie-info">
+        <h2>{title}</h2>
+        <p>{plot}</p>
+        <h4>{year}</h4>
+        <Link to="/">Back to Movies</Link>
+      </div>
+    </section>
+  );
+};
+
+export default SingleMovie;
