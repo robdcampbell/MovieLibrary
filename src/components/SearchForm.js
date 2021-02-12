fb80d6f7d6ce8104fcafcd0caed567507953a300
robdcampbell <rdeancampbell@gmail.com>
2021-02-11 23:49:12 -0500

    redoing button functionality

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useGlobalContext } from "../context";
 const SearchForm = () => {
   const { query, setQuery, error } = useGlobalContext();
+  const searchTerm = useRef();
 
   return (
-    <form
-      action=""
-      className="search-form"
-      onSubmit={(e) => e.preventDefault()}
-    >
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
       <h2>movie library.</h2>
-
-      <input
-        type="text"
-        className="form-input"
-        value={query}
-        onChange={(e) => {
-          setQuery(e.target.value);
-        }}
-      />
+      <div className="form__control">
+        <input
+          type="text"
+          className="form-input"
+          ref={searchTerm}
+          // value={query}
+          onChange={(e) => {
+            // setQuery(e.target.value);
+          }}
+          onSubmit={(e) => {
+            return setQuery(searchTerm.current.value);
+          }}
+        />
+        <button
+          type="button"
+          onClick={(e) => {
+            return setQuery(searchTerm.current.value);
+          }}
+        >
+          search
+        </button>
+      </div>
       {error.show && <div className="error">{error.msg}</div>}
     </form>
   );
